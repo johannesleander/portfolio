73d4597db7bd8f65535172849c190bb745e64b40
Johannes Leander <leanderjohannes@gmail.com>
2018-05-27 22:55:24 +0200

    Reusme routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ResumeComponent } from './resume/resume.component';
 
+const appRoutes: Routes = [
+    { path: 'resume', component: ResumeComponent }
+]
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ResumeComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ResumeComponent
+    ],
+    imports: [
+        RouterModule.forRoot(
+            appRoutes,
+            { enableTracing: true }
+        ),
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
